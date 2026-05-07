35762700d185eab581c8bcafb9409a3b85198546
Reiniel <reinieltalplacido529@gmail.com>
2026-05-07 17:54:43 +0800

    refactor: replace manual requires with PSR-4 autoloading and fix database imports

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
+require  __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
 
+use Framework\Router;
 
 $router = new Router();
 
